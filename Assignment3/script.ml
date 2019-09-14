180d52f2eecb11930be52c467fa196a48324995c
Ankit Solanki <ankit03june@gmail.com>
2019-09-15 04:57:42 +0530

    Wrote the function for invalid proof tre check

--- Assignment3/script.ml
+++ Assignment3/script.ml
@@ -56,24 +56,46 @@ type prop = T | F
 type judgement = Agree of (prop set) * prop;;
 
 type antecedents = Ant of judgement;;
+type consequent = Con of judgement;;
 
 type ndprooftree = End of judgement
-	| IntroImp of antecedents * ndprooftree
-	| ElimImp of (antecedents list) * ndprooftree
-	| Int of antecedents * ndprooftree
-	| Class of antecedents * ndprooftree
-	| IntroAnd of (antecedents list) * ndprooftree
-	| ElimAnd of (antecedents * ndprooftree) list
-	| IntroOr of (antecedents * ndprooftree) list
-	| ElimOr of (antecedents list) * ndprooftree;;
+	| IntroImp of antecedents * consequent * ndprooftree
+	| ElimImp of (antecedents list) * consequent * ndprooftree
+	| Int of antecedents * consequent * ndprooftree
+	| Class of antecedents * consequent * ndprooftree
+	| IntroAnd of (antecedents list) * consequent * ndprooftree
+	| ElimAnd of (antecedents * consequent * ndprooftree) list
+	| IntroOr of (antecedents * consequent * ndprooftree) list
+	| ElimOr of (antecedents list) * consequent * ndprooftree;;
 
 let rec check_if_concl_in_ass ass concl = match ass with
 	Set (a::rest) -> if a=concl then true else check_if_concl_in_ass (Set (rest)) concl
 	| Set ([]) -> false;; 
  
-
-let rec valid_ndprooftree pt = match pt with
-	End( Agree (ass, concl)) -> if concl=T then true
-								else (if (check_if_concl_in_ass ass concl) then true else false)
-	| Int (Ant (Agree (ass, concl)), childpt) -> if concl=F then valid_ndprooftree
-	
+let rec valid_ndprooftree pt g = match pt with
+	End (Agree (ass, concl)) -> if(g=ass) then 
+									(if concl=T then true
+									else (if (check_if_concl_in_ass ass concl) then true else false))
+								else false
+	| Int (Ant (Agree (ass1, concl1)), Con (Agree (ass2, concl2)), cpt) -> 
+			(equal ass1 ass2) && (equal ass1 g) && (concl1=F) &&  (valid_ndprooftree cpt g)
+	| Class (Ant (Agree ((Set(Not (r)::ass1)), concl1)), Con (Agree (ass2, concl2)), cpt) ->
+			(equal (Set (Not (r)::ass1)) g) && (concl1=F) && (equal ass2 (Set(ass1))) && (r=concl2) && (valid_ndprooftree cpt ass2)
+	| IntroImp (Ant (Agree (ass1, concl1)), Con (Agree (ass2, concl2)), cpt) ->
+			(equal ass1 g) &&
+				(match ass1 with Set(r::rest) -> (if (concl2=Impl(r, concl1) && (equal (Set(rest)) ass2)) 
+													then (valid_ndprooftree cpt ass2) else false))
+	| ElimImp (([Ant (Agree (ass1, Impl(p, q))); Ant (Agree (ass2, p1))]), Con (Agree (ass3, q1)), cpt) ->
+		(equal ass1 ass2) && (equal ass2 ass3) && 
+			(if (p=p1 && q=q1) then (valid_ndprooftree cpt ass3) else false)
+	| IntroAnd (([Ant (Agree (ass1, p)); Ant (Agree (ass2, q))]), Con (Agree (ass3, And(p1, q1))), cpt) ->
+		(equal ass1 ass2) && (equal ass2 ass3) && (p1=p && q1=q) && (valid_ndprooftree cpt ass3)
+	| ElimAnd ([(Ant (Agree (ass1, And(p, q))), Con ( Agree(ass2, p1)), cpt1) ; (Ant (Agree (ass3, And(p2, q2))), Con (Agree (ass4, q3)), cpt2)]) ->
+		(equal ass1 ass2) && (equal ass3 ass4) && (equal ass3 ass2) && (p=p1 && q2=q3 && p=p2 && q=q2)
+		&& (valid_ndprooftree cpt1 ass2) && (valid_ndprooftree cpt2 ass2)
+	| ElimOr (([Ant (Agree (ass1, And(p, q))); Ant (Agree (Set (p1::ass2), r1)); Ant (Agree (Set(q1::ass3), r2))]), Con (Agree (ass4, r3)), cpt) ->
+		(equal ass1 (Set (ass2))) && (equal (Set(ass2)) (Set(ass3))) & (equal (Set(ass3)) ass4)
+			& (p=p1 && q=q1 && r1=r2 && r2=r3) & (valid_ndprooftree cpt ass1)
+	| IntroOr ([(Ant (Agree (ass1, p)), Con ( Agree(ass2, Or(p1, q1))), cpt1) ; (Ant (Agree (ass3, q)), Con (Agree (ass4, Or(p2, q2))), cpt2)]) ->
+		(equal ass1 ass2) && (equal ass3 ass4) && (equal ass3 ass2) && (p1=p2 && q1=q2)
+		&& (((p=p1) && valid_ndprooftree cpt1 ass2) || ((q=q2) && valid_ndprooftree cpt2 ass2));;
